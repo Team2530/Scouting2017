37e7c6025ff6ec2cee178d18a6abffab5dbeccbb
nwdavidson <nwdavi09@smumn.edu>
2017-01-17 21:29:32 -0600

    finished up create functions

---
+++ js/database.js
@@ -0,0 +1,65 @@
+var databseOptions = {
+    fileName: "sqlite_scouting",
+    version: "1.0",
+    displayName: "SQLIte Scouting",
+    maxSize: 1024
+};
+
+var database = openDatabase(databseOptions.fileName,
+    databseOptions.version, databseOptions.displayName, databseOptions.maxSize);
+
+var createGeneralStatement = "CREATE TABLE general IF NOT EXISTS(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+    "team_num INTEGER, team_name TEXT, scout_name TEXT, match_num INTEGER, event TEXT, " +
+    "robot_play TEXT, cur_robo_rank INTEGER)";
+
+var createAutoStatement =  "CREATE TABLE auto IF NOT EXISTS(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+    "team_num INTEGER, match_num INTEGER, high_score INTEGER, low_score INTEGER, " +
+    "gear_del TEXT, move TEXT, penalty TEXT, cross_bl TEXT )";
+
+var createTeleopStatement = "CREATE TABLE tele_op IF NOT EXISTS(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+    "team_num INTEGER, match_num INTGER, high_score INTEGER, low_score INTEGER, floor_col TEXT, " +
+    "hopper_col TEXT, human_load TEXT, gears_del INTEGER, pickuo TEXT, pickup_hu TEXT, " +
+    "dropped_gear INTEGER, rope TEXT, penalties INTEGER)";
+
+var createCommentsStatement = "CREATE TABLE comments IF NOT EXISTS(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+    "team_num INTEGER, match_num INTEGER, comments NONE)";
+
+function createGeneralTable() {
+ database.transaction(function(tx){
+        tx.executeSql(createGeneralStatement);
+    }, function (error) {
+     console.log(error);
+ }, function () {
+     console.log("Created general ok");
+ });
+}
+
+function createAutoTable() {
+    database.transaction(function(tx){
+        tx.executeSql(createAutoStatement);
+    }, function (error) {
+        console.log(error);
+    }, function () {
+        console.log("Created general ok");
+    });
+}
+
+function createTeleopTable() {
+    database.transaction(function(tx){
+        tx.executeSql(createTeleopStatement);
+    }, function (error) {
+        console.log(error);
+    }, function () {
+        console.log("Created general ok");
+    });
+}
+
+function createCommentsTable() {
+    database.transaction(function(tx){
+        tx.executeSql(createCommentsStatement);
+    }, function (error) {
+        console.log(error);
+    }, function () {
+        console.log("Created general ok");
+    });
+}
--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
-var databseOptions = {
-    fileName: "sqlite_scouting",
-    version: "1.0",
-    displayName: "SQLIte Scouting",
-    maxSize: 1024
-};
-
-var database = openDatabase(databseOptions.fileName,
-    databseOptions.version, databseOptions.displayName, databseOptions.maxSize);
-
-var createGeneralStatement = "CREATE TABLE general IF NOT EXISTS(id INTEGER PRIMARY KEY AUTOINCREMENT," +
-    "team_num INTEGER, team_name TEXT, scout_name TEXT, match_num INTEGER, event TEXT, " +
-    "robot_play TEXT, cur_robo_rank INTEGER)";
-
-var createAutoStatement =  "CREATE TABLE auto IF NOT EXISTS(id INTEGER PRIMARY KEY AUTOINCREMENT," +
-    "team_num INTEGER, match_num INTEGER, high_score INTEGER, low_score INTEGER, " +
-    "gear_del TEXT, move TEXT, penalty TEXT, cross_bl TEXT )";
-
-var createTeleopStatement = "CREATE TABLE tele_op IF NOT EXISTS(id INTEGER PRIMARY KEY AUTOINCREMENT," +
-    "team_num INTEGER, match_num INTGER, high_score INTEGER, low_score INTEGER, floor_col TEXT, " +
-    "hopper_col TEXT, human_load TEXT, gears_del INTEGER, pickuo TEXT, pickup_hu TEXT, " +
-    "dropped_gear INTEGER, rope TEXT, penalties INTEGER)";
-
-var createCommentsTable = "CREATE TABLE comments IF NOT EXISTS(id INTEGER PRIMARY KEY AUTOINCREMENT," +
-    "team_num INTEGER, match_num INTEGER, comments NONE)";
+/**
+ * Created by stagg_000 on 1/17/2017.
+ */
